b9b9a0cbfecaa44993542c0bbf1788db231ec1a4
Luis Weich <weichluis.sw@gmail.com>
2021-05-27 21:59:34 +0200

    add login page

---
+++ ewul-frontend/src/main/webapp/pages/auth/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="auth-login">
+    <div class="center p-4">
+      <b-form-group label-cols-md="3" :label="$t('pages.auth.login.username')">
+        <b-form-input v-model="username" :readonly="loading" />
+      </b-form-group>
+      <b-form-group label-cols-md="3" :label="$t('pages.auth.login.password')">
+        <b-form-input v-model="password" :readonly="loading" />
+      </b-form-group>
+      <hr />
+      <div>
+        <b-button v-if="!loading" variant="primary" style="float: right;">
+          {{ $t('pages.auth.login.login') }}
+        </b-button>
+        <b-button v-else variant="primary" style="float: right;" disabled>
+          <b-spinner small class="mr-1" />
+          <span>{{ $t('basic.loading') }}</span>
+        </b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loading: false,
+      username: '',
+      password: ''
+    };
+  }
+};
+</script>
+
+<style scoped>
+.auth-login {
+  overflow: hidden;
+}
+.center {
+  margin: auto;
+  width: 50%;
+}
+</style>
