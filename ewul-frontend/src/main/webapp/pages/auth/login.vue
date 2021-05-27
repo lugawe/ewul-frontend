77ec25e56d378d7c24a261dea1569d53e128b5ea
Luis Weich <weichluis.sw@gmail.com>
2021-05-27 22:10:49 +0200

    login update

--- ewul-frontend/src/main/webapp/pages/auth/login.vue
+++ ewul-frontend/src/main/webapp/pages/auth/login.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="auth-login">
-    <div class="center p-4">
+    <b-form class="center p-4" @reset="reset" @submit="login">
       <b-form-group label-cols-md="3" :label="$t('pages.auth.login.username')">
-        <b-form-input v-model="username" :readonly="loading" />
+        <b-form-input v-model="username" :readonly="loading" required />
       </b-form-group>
       <b-form-group label-cols-md="3" :label="$t('pages.auth.login.password')">
-        <b-form-input v-model="password" :readonly="loading" />
+        <b-form-input v-model="password" :readonly="loading" required type="password" />
       </b-form-group>
       <hr />
-      <div>
-        <b-button v-if="!loading" variant="primary" style="float: right;">
+      <div class="bottom">
+        <b-button v-if="!loading" variant="primary" type="submit">
           {{ $t('pages.auth.login.login') }}
         </b-button>
-        <b-button v-else variant="primary" style="float: right;" disabled>
+        <b-button v-else variant="primary" disabled>
           <b-spinner small class="mr-1" />
           <span>{{ $t('basic.loading') }}</span>
         </b-button>
       </div>
-    </div>
+    </b-form>
   </div>
 </template>
 
@@ -30,16 +30,25 @@ export default {
       username: '',
       password: ''
     };
+  },
+  methods: {
+    reset() {
+      this.username = '';
+      this.password = '';
+    },
+    login(e) {
+      e.preventDefault();
+    }
   }
 };
 </script>
 
 <style scoped>
-.auth-login {
-  overflow: hidden;
-}
 .center {
   margin: auto;
   width: 50%;
+}
+.auth-login > .center > .bottom > .btn {
+  float: right;
 }
 </style>
