b96444dc714daee4dae0de16ba3e3e6f30fb8dc7
Luis Weich <weichluis.sw@gmail.com>
2021-07-14 18:58:36 +0200

    Input.js design update

--- ewul-frontend/components/Input.js
+++ ewul-frontend/components/Input.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default class Input extends React.Component {
   render() {
-    return <input placeholder={this.props.placeholder} />;
+    return (
+      <input
+        className='p-2 w-full content-center outline-none rounded-md border border-gray-200 focus:border-gray-50 focus:ring-1'
+        type={this.props.type || 'text'}
+        placeholder={this.props.placeholder || ''}
+      />
+    );
   }
 }
