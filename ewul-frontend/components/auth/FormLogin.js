760a115621badb314b184b9ff5e8682390e51de8
Luis Weich <weichluis.sw@gmail.com>
2021-10-14 21:03:33 +0200

    FormLogin login functionality

--- ewul-frontend/components/auth/FormLogin.js
+++ ewul-frontend/components/auth/FormLogin.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import * as UI from '@mui/material';
+import { login } from '../../api/auth/index';
 
 export default class FormLogin extends React.Component {
+  //
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  emailChange = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  passwordChange = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  login = async (e) => {
+    e.preventDefault();
+    const email = this.state.email;
+    const password = this.state.password;
+    const result = await login(email, password).catch((e) => {
+      // TODO
+    });
+  };
+
   render() {
     return (
       <ThemeProvider theme={theme}>
@@ -19,9 +47,25 @@ export default class FormLogin extends React.Component {
             <UI.Typography component='h1' variant='h4' sx={{ mb: 1 }}>
               <span>Sign in</span>
             </UI.Typography>
-            <UI.Box component='form'>
-              <UI.TextField margin='normal' required fullWidth label='E-Mail' type='email' />
-              <UI.TextField margin='normal' required fullWidth label='Password' type='password' />
+            <UI.Box component='form' onSubmit={this.login}>
+              <UI.TextField
+                onChange={this.emailChange}
+                value={this.state.email}
+                margin='normal'
+                required
+                fullWidth
+                label='E-Mail'
+                type='email'
+              />
+              <UI.TextField
+                onChange={this.passwordChange}
+                value={this.state.password}
+                margin='normal'
+                required
+                fullWidth
+                label='Password'
+                type='password'
+              />
               <UI.FormControlLabel control={<UI.Checkbox />} label='Remember me' />
               <UI.Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
                 <span>Sign In</span>
